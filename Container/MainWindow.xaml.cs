b13cde197f5277e00ec0f67afcf62d358ec02a1a
KlaasHagge <klaash@ppedv.de>
2021-06-16 09:51:36 +0200

    Layoutcontainer

---
+++ Container/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Container
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Codeseitiger Schreib-Zugriff auf eine Attached-Property des Canvas
+            Canvas.SetLeft(Rct_Blue, 50);
+        }
+    }
+}
