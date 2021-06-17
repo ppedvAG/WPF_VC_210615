04bea746a280f6910ef086abd6220ff6061b88d9
KlaasHagge <klaash@ppedv.de>
2021-06-17 11:38:43 +0200

    DataBinding (kommentiert)

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Show_Click(object sender, RoutedEventArgs e)
+        {
+            //Ausgabe der Vorname-Property
+            MessageBox.Show((Spl_DataContextBsp.DataContext as Person).Vorname);
+        }
+
+        private void Btn_Altern_Click(object sender, RoutedEventArgs e)
+        {
+            //Erhöhung des Alters der Person im DataContextes des StackPanels
+            (Spl_DataContextBsp.DataContext as Person).Alter++;
+        }
+    }
+}
---
+++ DataBinding/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    //Das Interface INotifyPropertyChanged sorgt für ein neues Event, welches bei Aktivierung die GUI über eine Veränderung in diesem Objekt informiert
+    public class Person : INotifyPropertyChanged
+    {
+        //Eine Datenbindung kann nur an Properties durchgeführt werden (keine Felder)
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        private int alter;
+        public int Alter
+        {
+            get { return alter; }
+            set
+            {
+                alter = value;
+                //Das PropertyChanged-Event muss zu dem Zeitpunkt geworfen werden, zu dem die GUI über eine Veränderung informiert werden soll
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public List<DateTime> WichtigeTage { get; set; } = new List<DateTime>()
+        {
+            new DateTime(2003, 12, 3)
+        };
+
+        //Durch das Interface geforderte Event
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
